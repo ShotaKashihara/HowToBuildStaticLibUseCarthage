6fa99999853c50cdbd079ce4c99160ee2df710e2
Shota Kashihara <shota.kashihara.1004@gmail.com>
2018-12-22 23:27:10 +0900

    Initial Commit

---
+++ HowToUseStaticLibrary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HowToUseStaticLibrary
+//
+//  Created by Shota Kashihara on 2018/12/22.
+//  Copyright © 2018 Shota Kashihara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
